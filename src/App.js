19f741b1be2e1b1139fd6f40f82c168ad8192e07
Nidhi Sharma <nisharma@gainsight.com>
2023-11-24 15:03:50 +0530

    structure changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "nidhis-webc";
+function App() {
+  return (
+    <div className="App">
+      <hello-world data-text="nidhi "></hello-world>
+      <maths-mult data-number1="3" data-number2="7"></maths-mult>
+    </div>
+  );
+}
+
+export default App;
